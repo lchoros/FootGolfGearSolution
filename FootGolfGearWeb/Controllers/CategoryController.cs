d84e35f237c19f5b4ffe263fc99a6bad1ea9a05d
Lukasz Choros <lchoros7@gmail.com>
2024-09-19 22:18:02 +0100

    Add bootstrap, botswatch, seed categories, create, display and validate categories

---
+++ FootGolfGearWeb/Controllers/CategoryController.cs
@@ -0,0 +1,42 @@
+﻿using FootGolfGearWeb.Data;
+using FootGolfGearWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FootGolfGearWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;        
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categoryList = _db.Categories.ToList();
+            return View(categoryList);
+        }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            //if(obj.Name ==  obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("name", "The Display Order cannot excatly match the Name.");
+            //}
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ FootGolfGearWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using FootGolfGearWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FootGolfGearWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "FGB", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Pro Balls", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Accessories", DisplayOrder = 3 }
+                );
+        }
+    }
+}
